f9fecde1cc5ddb725abe8b3adc8fc8e08c0ab825
marc <marc@dabadisseny.com>
2011-10-13 17:26:10 +0200

    v1.1

--- daba-galeria.js
+++ daba-galeria.js
@@ -1,14 +1,18 @@
 var $dabaGaleria=jQuery.noConflict();
 $dabaGaleria(document).ready(function(){
-    Galleria.loadTheme('wp-content/plugins/DaBaDisseny-Galeria/galleria/themes/classic/galleria.classic.js');
+    var hostname = window.location.hostname;
+    var protocol = window.location.protocol;
+    var destination = protocol+'//'+hostname+'/'+'wp-content/plugins/DaBaDisseny-Galeria/galleria/themes/classic/galleria.classic.js';
+    Galleria.loadTheme(destination);
     $dabaGaleria(".daba-galeria").each(function(){
         var w=$dabaGaleria(this).width();
         var h=$dabaGaleria(this).height();
         $dabaGaleria(this).galleria({
             width: w,
-            height: h
+            height: h,
+            autoplay: 5000,
+            debug: false
         });
-
     });
     $dabaGaleria('.daba-galeria:odd').addClass('dabaGaleriaSenar');
     $dabaGaleria('.daba-galeria:even').addClass('dabaGaleriaParell');
